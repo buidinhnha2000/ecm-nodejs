c12e9318d0352b08eaf2fa4a11dfdd1d3d3ce19f
bdnha <bdnha.18it3@gmail.com>
2023-02-21 10:26:45 +0700

    node

--- routes/cart.js
+++ routes/cart.js
@@ -5,13 +5,22 @@ const router = Router();;
 
 //CREATE
 router.post("/", async (req, res) => {
-  const newCart = new Cart(req.body);
-  try {
-    const savedCart = await newCart.save();
-    res.status(200).json(savedCart);
-  } catch (err) {
-    res.status(500).json(err);
+  console.log(req.body)
+  const { productId, userId } = req.body;
+  
+  const cart = await Cart.findOne({productId, userId });
+  console.log(cart)
+  if (cart == null) {
+    const newCart = new Cart(req.body);
+    await newCart.save();
+    res.json(newCart.populate('productId'))
+    return;
   }
+
+  cart.quantity = cart.quantity + 1;
+  await cart.save()
+
+  res.json(cart.populate('productId'))
 });
 
 //UPDATE
@@ -53,8 +62,9 @@ router.delete("/find/:id", async (req, res) => {
 //GET USER CART
 router.get("/find/:userId", async (req, res) => {
   try {
-    const cart = await Cart.find({ userId: req.params.userId });
+    const cart = await Cart.find({ userId: req.params.userId }).populate('productId');
     res.status(200).json(cart);
+    console.log(cart)
   } catch (err) {
     res.status(500).json(err);
   }
